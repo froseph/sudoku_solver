60ab97aed3e7842068ebd835fbdbe2ed7380c37d
Joseph Liu <froseph@gmail.com>
2021-08-22 12:29:11 -0700

    Implement recusive solver for sudoku

--- lib/sudoku_solver.ex
+++ lib/sudoku_solver.ex
@@ -15,4 +15,46 @@ defmodule SudokuSolver do
   def hello do
     :world
   end
+
+  def solve_recusive(%SudokuBoard{size: size} = board) do
+    max_index = size * size - 1
+    solve_recursive_helper(board, max_index)
+  end
+
+  defp solve_recursive_helper(%SudokuBoard{} = board, -1) do
+    if SudokuBoard.solved?(board) do
+      b
+    else
+      nil
+    end
+  end
+  defp solve_recursive_helper(%SudokuBoard{size: size, grid: grid} = board, idx) do
+    elt = Enum.at(grid, idx)
+    if elt != 0 do
+      solve_recursive_helper(board, idx-1)
+    else
+      try_solve_recursive(board, idx, Enum.to_list(1..size))
+    end
+  end
+
+  defp try_solve_recursive(%SudokuBoard{}, idx, []) do
+    nil
+  end
+  defp try_solve_recursive(%SudokuBoard{grid: grid} = board, idx, [number | others]) do
+    new_board = SudokuBoard.place_number(board, idx, number)
+    if SudokuBoard.partial_solution?(new_board) do
+      solution = solve_recursive_helper(new_board, idx + 1)
+      if solution == nil do
+        try_solve_recursive(board, idx, others)
+      else
+        solution
+      end
+    else
+      try_solve_recursive(board, idx, others)
+    end
+  end
+
+  def solve_cps do
+    nil
+  end
 end
