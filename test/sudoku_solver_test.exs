93ee9d340da42aaee59f9b55bfff7aef5152599d
Joseph Liu <froseph@gmail.com>
2021-08-24 10:17:12 -0700

    Add testing for SudokuSolver

--- test/sudoku_solver_test.exs
+++ test/sudoku_solver_test.exs
@@ -2,7 +2,33 @@ defmodule SudokuSolverTest do
   use ExUnit.Case
   doctest SudokuSolver
 
-  test "greets the world" do
-    assert SudokuSolver.hello() == :world
+  test "solve sudoku using recursion" do
+    {_, input} = SudokuBoard.read_file("./test/s01a.txt")
+    {_, expected} = SudokuBoard.read_file("./test/s01a_s.txt")
+
+    solution = SudokuSolver.solve_recursive(input)
+    assert expected == solution
+  end
+
+  test "solve impossible sudoku using recursion" do
+    {_, input} = SudokuBoard.read_file("./test/unsolveable.txt")
+
+    solution = SudokuSolver.solve_recursive(input)
+    assert nil == solution
+  end
+
+  test "solve sudoku using cps" do
+    {_, input} = SudokuBoard.read_file("./test/s01a.txt")
+    {_, expected} = SudokuBoard.read_file("./test/s01a_s.txt")
+
+    solution = SudokuSolver.solve_cps(input)
+    assert expected == solution
+  end
+
+  test "solve impossible sudoku using cps" do
+    {_, input} = SudokuBoard.read_file("./test/unsolveable.txt")
+
+    solution = SudokuSolver.solve_cps(input)
+    assert nil == solution
   end
 end
